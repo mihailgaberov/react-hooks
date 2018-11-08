e8d5c35721c2317a23aa07e3338ea2a8183a94be
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-11-08 11:03:58 +0100

    Updated styling.

--- src/styled/StyledRow.js
+++ src/styled/StyledRow.js
@@ -2,7 +2,23 @@ import styled from 'styled-components'
 
 const StyledRow = styled.div`
   margin: 14px;
+  padding: 14px;
   border: 1px solid rebeccapurple;
+
+  ul {
+    list-style-type: none;
+  }
+
+  .special {
+    font-size: 2em;
+    font-family: Consolas;
+    color: pink;
+    background-color: white;
+  }
+
+  @media (min-width: 800px) {
+    width: 100%;
+  }
 `;
 
 export default StyledRow
