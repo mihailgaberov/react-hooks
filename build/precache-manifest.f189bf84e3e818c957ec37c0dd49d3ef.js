f26fc8a7a91bad5d76b8f0a3e04d2a670e07d9fd
Mihail_Gaberov <Mihail_Gaberov@epam.com>
2019-03-11 17:21:14 +0100

    Added build folder to git.

---
+++ build/precache-manifest.f189bf84e3e818c957ec37c0dd49d3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c7de7f8e497ccd52e05",
+    "url": "/static/css/main.d4963118.chunk.css"
+  },
+  {
+    "revision": "5c7de7f8e497ccd52e05",
+    "url": "/static/js/main.2632efbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "764630059709a2bab41f",
+    "url": "/static/js/2.d7acf61f.chunk.js"
+  },
+  {
+    "revision": "fcd388b32c3b9b98ab720a6ce5618d48",
+    "url": "/index.html"
+  }
+];
