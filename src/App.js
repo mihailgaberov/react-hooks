92bff11d9556f95fd5a45fc5d09312a4d03dd3b6
Mihail_Gaberov <Mihail_Gaberov@epam.com>
2019-03-11 17:03:24 +0100

    Added styling.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ const App = () => {
     fetchPictures(10);
   }, []);
 
+
+  // Separate effects for achieving Separation of Concerns
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  });
+
   async function fetchPictures(count) {
     const response = await API.post('shibaapi', '/pictures', { body: { number: count } });
     setPictures(response.data);
@@ -21,21 +27,17 @@ const App = () => {
   const renderPics = (pictures) => (pictures && pictures.map((el, idx) => (
     <div key={idx}><img src={el} alt='Shibe' title='Shibe' /></div>)));
 
-  function getRandomColor() {
+  const getRandomColor = () => {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
-
-  useEffect(() => {
-    document.body.style.backgroundColor = color;
-  });
+  };
 
   return <>
-    <h2>useEffect</h2>
+    <h2>useState() & useEffect()</h2>
     <button onClick={() => setColor(getRandomColor())}>Change Background</button>
     <button onClick={() => fetchPictures(10)}>Fetch More</button>
     <div className='images-container'>
