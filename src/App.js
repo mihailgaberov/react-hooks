30de6842399e833c5298c3459210655629e6d1ed
mihailgaberov <mihail.gaberov@gmail.com>
2019-03-22 13:03:31 +0100

    Added a custom hook for fetching images.

--- src/App.js
+++ src/App.js
@@ -8,21 +8,27 @@ Amplify.configure(awsmobile);
 /*
 * If a function doesn’t use anything from the component scope,
 * you can hoist it outside the component and then freely use it inside your effects
-* */
-
-async function fetchPictures(count) {
-  const response = await API.post('shibaapi', '/pictures', { body: { number: count } });
-  return response.data;
-}
-
-const App = () => {
+*
+/* A custom hooks for fetching `count` images */
+function usePictures(count) {
   const [pictures, setPictures] = useState([]);
-  const [color, setColor] = useState('#DDD');
 
   useEffect(() => {
-    fetchPictures(10).then(setPictures);
+    async function fetchPictures() {
+      const response = await API.post('shibaapi', '/pictures', { body: { number: count } });
+      setPictures(response.data);
+    }
+
+    fetchPictures(10);
   }, []);
 
+  return pictures;
+}
+
+
+const App = () => {
+  const [color, setColor] = useState('#DDD');
+  const pictures = usePictures(10);
 
   // Separate effects for achieving Separation of Concerns
   useEffect(() => {
@@ -33,18 +39,16 @@ const App = () => {
     <div key={idx}><img src={el} alt='Shibe' title='Shibe' /></div>)));
 
   const getRandomColor = () => {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
   };
 
   return <>
-    <h2>useState() & useEffect()</h2>
     <button onClick={() => setColor(getRandomColor())}>Change Background</button>
-    <button onClick={() => fetchPictures(10)}>Fetch More</button>
     <div className='images-container'>
       {renderPics(pictures)}
     </div>
